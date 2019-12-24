05345d6a7c6dd4b003dadf1afb172c9fc71fb613
unknown <wangchuanyi-gm@gome.com.cn>
2019-12-24 15:00:57 +0800

    update

--- autoCommit.sh
+++ autoCommit.sh
@@ -2,13 +2,11 @@
 # 这是一个定时提交代码的脚本
 # 
 # 功能：定时提交代码
-# 特色：依赖 linux 让 crontab 
-# 0 18 * * * /home/testuser/test.sh
+# 特色：依赖 linux 让 crontab  -e
+# 0 18 * * * sh /home/code/weekly/autoCommit.sh
 #--------------------------------------------
-cd /d D:\github\weekly
+cd /home/code/weekly
 git pull
-cd /d D:\github\weekly
 git add -A .
 git commit -m "update"
 git push origin master
-echo "weekly ok "
