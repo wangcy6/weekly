02842c8b93ea67ef9ce11888b7e97e12403bc57b
wang_cyi <wang_cyi@163.com>
2020-01-17 18:51:31 +0800

    update

--- update_github.bat
+++ update_github.bat
@@ -1,42 +1,7 @@
 @echo off
-
-cd /d F:\coding\weekly
 git pull
 git add -A .
 git commit -m "update"
 
-echo "commit weekly ok"
-
-cd /d F:\coding\leetcode
-git pull
-git add -A .
-git commit -m "update"
-echo "leetcodeok"
-
-cd /d F:\coding\reading_code_note
-git pull
-git add -A .
-git commit -m "update"
-echo "reading_code_note ok"
-
-
-cd /d G:\money\eye
-git pull
-git add -A .
-git commit -m "update"
-echo "eye ok"
-
-
-cd /d G:\code\TryEverything
-git pull
-git add -A .
-git commit -m "update"
-echo "TryEverything ok"
-
-cd /d F:\coding\leetcode
-git pull
-git add -A .
-git commit -m "update"
-echo "TryEverything ok"
 
 
