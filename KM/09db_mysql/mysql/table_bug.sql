179c4d01c9aea63e3683d551581c38e1ba94b0d0
wang_cyi <wang_cyi@163.com>
2020-04-17 09:30:22 +0800

    update

--- KM/09db_mysql/mysql/table_bug.sql
+++ KM/09db_mysql/mysql/table_bug.sql
@@ -1,4 +1,5 @@
 
+insert into bug(problem,tag,failed,try,fix) values('自己付出行动，但是没有收获，自己开始抱怨，行动失误，后面不在行动了，这个不对地方，你应该继续行动 知道成功',1000,0,0);
 
 insert into bug(problem,tag,failed,try,fix) values('你存在一个问题，你说半天别人根本同意，你放弃说，认真听 认真听别人意思，不急着发表不同的意见','沟通双方表达不是一个意思',1000,0,0);
 
@@ -6,3 +7,5 @@ insert into bug(problem,tag,failed,try,fix) values('只要手放到鼠标上你�
 
 
 insert into bug(problem,tag,failed,try,fix) values('9个因素一起来 a手机有百度网盘，b在周末时候，c有热门电影，综艺节目，d在吃饭时候，e 在床上，f未来不控制，g微信有资源，h手机解锁 i 地铁 让思想开始堕落。不停看手机很忙碌','自己放弃的后果',1000,0,0);
+
+
